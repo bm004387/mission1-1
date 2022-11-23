43cb6883e1480724a1c4318103b6daf9e0661535
bm004387 <bm004387@gmail.com>
2022-11-23 17:17:46 +0900

    join js commit

--- javascript/script.js
+++ javascript/script.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
+    //  마우스를 오버(올렸을)했을 때 다른 작업을 멈추고  slideDown한다. 500ms = 0.5s = 500밀리초 = 0.5초
     $('.navi > li').mouseover(function(){
         $(this).find('.submenu').stop().slideDown(500);
     }).mouseout(function(){
         $(this).find('.submenu').stop().slideUp(500);
     });
+    
     $('.imgslide a:gt(0)').hide();
      setInterval(function(){
         $('.imgslide a:first-child').fadeOut()
